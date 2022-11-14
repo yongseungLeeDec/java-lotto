8f3cbe60829c29420ad927c512b9adb12e42020a
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-14 19:19:26 +0900

    feat: 구입한 로또 개수만큼 응모 번호 배열 출력 기능 구현
    PurchaseResultDemonstrator의 printPurchasedAmount 메서드를 구현함.
    - 해당 메서드는 PurchaseResultProcessor가 생성한 새로운 로또 리스트를 순회하며 로또 개체를 문자열로 콘솔에 출력함.

--- src/main/java/lotto/PurchaseResultDemonstrator.java
+++ src/main/java/lotto/PurchaseResultDemonstrator.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class PurchaseResultDemonstrator {
     private static final String PURCHASED_AMOUNT_NOTICE = "개를 구매했습니다.";
 
@@ -7,4 +9,9 @@ public class PurchaseResultDemonstrator {
         System.out.println(purchasedAmount + PURCHASED_AMOUNT_NOTICE);
     }
 
+    public void printPurchasedLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
 }
