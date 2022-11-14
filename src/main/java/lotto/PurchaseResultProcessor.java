75e9498f052281c83858f50ba9efde7b0610311c
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-14 18:26:11 +0900

    feat: 새로운 로또를 발행(생성)하는 기능 구현
    PurchaseResultProcessor의 issueNewLotto 메서드를 구현함.
    - 해당 메서드는 내부에서 새로운 로또 개체를 생성하여 반환함.

--- src/main/java/lotto/PurchaseResultProcessor.java
+++ src/main/java/lotto/PurchaseResultProcessor.java
@@ -1,8 +1,24 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class PurchaseResultProcessor {
+    private static final int LOTTO_START_NUMBER_INCLUSIVE = 1;
+    private static final int LOTTO_END_NUMBER_INCLUSIVE = 45;
+    private static final int NUMBER_OF_PICKED_NUMBERS = 6;
 
     public int getPurchasedAmount(int totalPrice) {
         return totalPrice / 1000;
+    }
+
+    private Lotto issueNewLotto() {
+        List<Integer> randomNumbers = pickUniqueNumbersInRange(
+                LOTTO_START_NUMBER_INCLUSIVE, LOTTO_END_NUMBER_INCLUSIVE, NUMBER_OF_PICKED_NUMBERS
+        );
+        Collections.sort(randomNumbers);
+        return new Lotto(randomNumbers);
     }
 }
