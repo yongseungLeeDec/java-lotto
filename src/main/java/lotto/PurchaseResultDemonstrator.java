2d87d700ccfe2d849184d56361a71d2e35a75f9c
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-14 19:16:51 +0900

    feat: 구입한 로또 개수 출력 기능 구현
    PurchaseResultDemonstrator의 printPurchasedAmount 메서드를 구현함.
    - 해당 메서드는 사용자가 구입한 로또 개수를 형식화된 문자열로 콘솔에 출력함.

--- src/main/java/lotto/PurchaseResultDemonstrator.java
+++ src/main/java/lotto/PurchaseResultDemonstrator.java
@@ -1,5 +1,10 @@
 package lotto;
 
 public class PurchaseResultDemonstrator {
+    private static final String PURCHASED_AMOUNT_NOTICE = "개를 구매했습니다.";
+
+    public void printPurchasedAmount(int purchasedAmount) {
+        System.out.println(purchasedAmount + PURCHASED_AMOUNT_NOTICE);
+    }
 
 }
