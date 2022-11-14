5ebb8520f6ac4d1c6fe7303aaad2d15a76082f53
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-14 14:53:40 +0900

    feat: 사용자 입력 첫 문자 1~9 여부 확인 기능 구현
    - UserInputValidator의 isFirstLetterBetweenOneAndNine 메서드를 구현함.
    - 해당 메서드는 사용자 입력의 가장 첫 문자가 1~9 사이의 숫자(numeric literal)인지 여부를 boolean 타입으로 반환함.

--- src/main/java/lotto/UserInputValidator.java
+++ src/main/java/lotto/UserInputValidator.java
@@ -10,4 +10,9 @@ public abstract class UserInputValidator {
         }
         return true;
     }
+
+    public static boolean isFirstLetterBetweenOneAndNine(String userInput) {
+        char firstLetter = userInput.charAt(0);
+        return firstLetter >= '1' && firstLetter <= '9';
+    }
 }
