b17b5a37f6e56edfc77b62707132de03272e8242
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-14 14:44:37 +0900

    fix(delete): Lotto 클래스 기능 삭제
    Lotto 클래스에 구현된 어플리케이션 기능을 다른 클래스로 분리하기 위해 삭제함.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,10 +2,7 @@ package lotto;
 
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Lotto {
-    private static final String PROMPT_TOTAL_PRICE = "구입금액을 입력해 주세요.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,13 +14,5 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-    }
-
-    private void promptUserForTotalPrice() {
-        System.out.println(PROMPT_TOTAL_PRICE);
-    }
-
-    private String getTotalPriceFromUser() {
-        return readLine();
     }
 }
