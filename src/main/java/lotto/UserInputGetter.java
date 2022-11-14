d6eedb50cd00241aa35ab6c427b042618efd5e8a
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-14 18:31:59 +0900

    fix(modify): UserInputGetter 메서드 접근제어자 수정
    UserInputGetter 클래스 메서드들의 접근제어자가 private로 되어 있어 public으로 수정함.

--- src/main/java/lotto/UserInputGetter.java
+++ src/main/java/lotto/UserInputGetter.java
@@ -5,11 +5,11 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class UserInputGetter {
     private static final String PROMPT_TOTAL_PRICE = "구입금액을 입력해 주세요.";
 
-    private void promptUserForTotalPrice() {
+    public void promptUserForTotalPrice() {
         System.out.println(PROMPT_TOTAL_PRICE);
     }
 
-    private String getTotalPriceFromUser() {
+    public String getTotalPriceFromUser() {
         return readLine();
     }
 }
