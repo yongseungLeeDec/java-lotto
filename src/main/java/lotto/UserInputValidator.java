d9aa41e63c78ac6ca87e712948b678d17e6d6982
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-14 14:52:33 +0900

    feat: 사용자 입력의 올바른 숫자 형식 여부 확인 기능 구현
    - UserInputValidator의 isUserInputValidNumberFormat 메서드를 구현함.
    - 해당 메서드는 사용자 입력이 올바른 Number Format에 해당하는지 boolean 타입으로 반환함.

--- src/main/java/lotto/UserInputValidator.java
+++ src/main/java/lotto/UserInputValidator.java
@@ -2,4 +2,12 @@ package lotto;
 
 public abstract class UserInputValidator {
 
+    public static boolean isUserInputValidNumberFormat(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException exception) {
+            return false;
+        }
+        return true;
+    }
 }
