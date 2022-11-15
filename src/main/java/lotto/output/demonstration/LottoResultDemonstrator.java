2a6836c3cd9bd7c3956962dbd4f22c4a8365e045
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 20:18:30 +0900

    feat: 수익률 출력 기능 구현
    LottoResultDemonstrator의 printProfitRate 메서드를 구현함.
    - LottoResultEvaluator에서 계산한 수익률을 일정한 형식의 문구와 함께 콘솔에 출력함.

--- src/main/java/lotto/output/demonstration/LottoResultDemonstrator.java
+++ src/main/java/lotto/output/demonstration/LottoResultDemonstrator.java
@@ -5,6 +5,8 @@ import lotto.data.LottoResult;
 public class LottoResultDemonstrator {
     private static final String HEADING = "당첨 통계";
     private static final String BREAK_LINE = "---";
+    private static final String PROFIT_RATE_PHRASE_HEAD = "총 수익률은 ";
+    private static final String PROFIT_RATE_PHRASE_TAIL = "%입니다.";
 
     public void printLottoResult(LottoResult lottoResult) {
         printHeading();
@@ -18,5 +20,9 @@ public class LottoResultDemonstrator {
 
     private void printBreakLine() {
         System.out.println(BREAK_LINE);
+    }
+
+    public void printProfitRate(double profitRate) {
+        System.out.println(PROFIT_RATE_PHRASE_HEAD + profitRate + PROFIT_RATE_PHRASE_TAIL);
     }
 }
