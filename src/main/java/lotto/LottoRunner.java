41e83b7e84af8bac34d26315015f75e027f978fa
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 19:27:30 +0900

    feat: LottoRunner에 당첨/보너스 번호 입력 기능 코드 추가

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import lotto.data.LottoNumber;
+import lotto.data.LottoResult;
 import lotto.data.PurchaseResult;
+import lotto.input.LottoNumberGetter;
+import lotto.output.calculation.LottoResultEvaluator;
+import lotto.output.demonstration.LottoResultDemonstrator;
 import lotto.output.demonstration.PurchaseResultDemonstrator;
 import lotto.output.calculation.PurchaseResultProcessor;
 import lotto.input.TotalPriceGetter;
@@ -11,11 +16,13 @@ public class LottoRunner {
     private final TotalPriceGetter totalPriceGetter;
     private final PurchaseResultDemonstrator purchaseResultDemonstrator;
     private final PurchaseResultProcessor purchaseResultProcessor;
+    private final LottoNumberGetter lottoNumberGetter;
 
     private LottoRunner() {
         this.totalPriceGetter = new TotalPriceGetter();
         this.purchaseResultDemonstrator = new PurchaseResultDemonstrator();
         this.purchaseResultProcessor = new PurchaseResultProcessor();
+        this.lottoNumberGetter = new LottoNumberGetter();
     }
 
     public static LottoRunner getLottoRunner() {
@@ -32,7 +39,8 @@ public class LottoRunner {
         PurchaseResult purchaseResult = this.purchaseResultProcessor.getPurchaseResult(totalPrice);
         this.purchaseResultDemonstrator.printPurchaseResult(purchaseResult);
         System.out.println();
+
+        LottoNumber lottoNumber = this.lottoNumberGetter.getLottoNumber();
+        System.out.println();
     }
-
-
 }
