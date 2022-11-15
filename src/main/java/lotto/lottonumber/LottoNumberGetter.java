33b61781796d055eaccb08e7a860d662eb68dd23
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:43:40 +0900

    feat: 보너스 번호 입력이 유효하지 않을 경우 예외 발생시키는 기능 구현

--- src/main/java/lotto/lottonumber/LottoNumberGetter.java
+++ src/main/java/lotto/lottonumber/LottoNumberGetter.java
@@ -1,6 +1,8 @@
 package lotto.lottonumber;
 
 
+import lotto.lottonumber.bonusnumber.BonusNumberValidationCode;
+import lotto.lottonumber.bonusnumber.BonusNumberValidator;
 import lotto.lottonumber.winningnumber.WinningNumberValidationCode;
 import lotto.lottonumber.winningnumber.WinningNumberValidator;
 
@@ -15,9 +17,11 @@ public class LottoNumberGetter {
     private static final String PROMPT_FOR_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     private final WinningNumberValidator winningNumberValidator;
+    private final BonusNumberValidator bonusNumberValidator;
 
     public LottoNumberGetter() {
         this.winningNumberValidator = new WinningNumberValidator();
+        this.bonusNumberValidator = new BonusNumberValidator();
     }
 
     public void printPromptForWinningNumbers() {
@@ -50,6 +54,13 @@ public class LottoNumberGetter {
     public void throwExceptionIfNotValidWinningNumbers(List<String> tokens) {
         WinningNumberValidationCode result = this.winningNumberValidator.validateWinningNumber(tokens);
         if (result != WinningNumberValidationCode.VALID) {
+            throw new IllegalArgumentException(result.getErrorMessage());
+        }
+    }
+
+    public void throwExceptionIfNotValidBonusNumber(String token, List<Integer> winningNumbers) {
+        BonusNumberValidationCode result = this.bonusNumberValidator.validateBonusNumber(token, winningNumbers);
+        if (result != BonusNumberValidationCode.VALID) {
             throw new IllegalArgumentException(result.getErrorMessage());
         }
     }
