7721291d56039c54e3348521ae563f055c76d6e2
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 20:05:43 +0900

    feat: 맞춘 숫자 개수별 로또의 개수를 세는 기능 구현
    LottoResultEvaluator 클래스의 countLottosByWinningResult() 메서드를 구현함.
    - 해당 메서드는 구매한 로또별 결과(맞춘 숫자 개수)를 담은 배열을 인자로 받음.
    - 맞춘 숫자 개수별로 로또의 개수를 세어, 그 결과를 배열에 담아 반환함.
    - 반환되는 배열의 색인은 곧 맞춘 숫자의 개수(N개)이며,
    - 해당 색인에 있는 값은 N개 숫자를 맞춘 로또의 개수를 뜻함.

--- src/main/java/lotto/output/calculation/LottoResultEvaluator.java
+++ src/main/java/lotto/output/calculation/LottoResultEvaluator.java
@@ -2,11 +2,23 @@ package lotto.output.calculation;
 
 import lotto.data.Lotto;
 import lotto.data.LottoNumber;
+import lotto.data.LottoResult;
 
 import java.util.List;
 
 public class LottoResultEvaluator {
     private static final int NUMBER_TO_EXPRESS_FIVE_AND_BONUS_WIN = 7;
+
+    public int[] countLottosByWinningResult(int[] winningResults) {
+        int[] countsByWinningResult = new int[8];
+
+        for (int i = 0; i < winningResults.length; i++) {
+            int numberMatched = winningResults[i];
+            countsByWinningResult[numberMatched] += 1;
+        }
+
+        return countsByWinningResult;
+    }
 
     public int LottoResultEvaluator(LottoNumber lottoNumber, Lotto purchasedLotto) {
         List<Integer> winningNumbers = lottoNumber.getLotto().getNumbers();
