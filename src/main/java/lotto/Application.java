302ff6d4f43bc7876e56192e9f08af9e30e605b8
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:13:33 +0900

    fix(delete): 이전 커밋 오류 내용 삭제
    - 이전 커밋에서 Application 클래스의 수정 내용이 함께 반영되어 이를 삭제함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,8 @@
 package lotto;
 
 public class Application {
-    private static final String ERROR_PREFIX = "[ERROR] ";
 
     public static void main(String[] args) {
-        try {
-            LottoRunner.getLottoRunner().run();
-        } catch (IllegalArgumentException exception) {
-            System.out.println(ERROR_PREFIX + exception.getMessage());
-        }
+
     }
 }
