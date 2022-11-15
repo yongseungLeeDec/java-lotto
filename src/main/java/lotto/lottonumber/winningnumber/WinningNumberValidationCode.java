706f31b493e8edfa177d48785c12f33dd0f35d98
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:20:22 +0900

    feat: 당첨번호 입력 검증코드 열거형 WinningNumberValidationCode 구현
    LottoGetter 클래스에서 입력 받은 당첨 번호가 올바른 형식인지 결과를 검증코드로 나타내기 위한 WinningNumberValidationCode 열거형 클래스를 구현함.

---
+++ src/main/java/lotto/lottonumber/winningnumber/WinningNumberValidationCode.java
@@ -0,0 +1,19 @@
+package lotto.lottonumber.winningnumber;
+
+public enum WinningNumberValidationCode {
+    HAS_MORE_OR_LESS_THAN_SIX_TOKENS("입력된 숫자(또는 단어)가 여섯 개 미만 또는 초과입니다."),
+    HAS_ILLEGAL_CHARACTERS("1 ~ 45 사이 외 수를 제외한 어떠한 입력도 허용하지 않습니다."),
+    DUPLICATE_NUMBER("당첨 번호는 모두 서로 다른 6개의 숫자여야 합니다"),
+    NUMBER_OUT_OF_RANGE("당첨 번호에 허용 범위를 벗어난 수가 포함되어 있습니다. 1 ~ 45 사이의 수만 허용합니다."),
+    VALID("올바른 입력입니다.");
+
+    private final String errorMessage;
+
+    WinningNumberValidationCode(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
