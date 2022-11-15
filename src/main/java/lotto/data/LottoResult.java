01404199a8273f17f6070fc7e5b11c54a23063a8
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 19:43:34 +0900

    feat: 당첨금 총액을 계산하는 기능 구현
    LottoResult의 getTotalPrizeAmount를 구현함.
    - 해당 클래스는 저장하고 있는 멤버 변수와 WinningResult 열거형에 저장된 단위 당첨금 정보를 조합해, 해당 당첨 통계(LottoResult)의 당첨금 총액을 계산해서 반환함.

--- src/main/java/lotto/data/LottoResult.java
+++ src/main/java/lotto/data/LottoResult.java
@@ -17,5 +17,14 @@ public class LottoResult {
         this.lottosWonSix = countResults[6];
     }
 
+    public int getTotalPrizeAmount() {
+        int prize = 0;
+        prize += lottosWonThree * WinningResult.THREE.getPrizeAmount();
+        prize += lottosWonFour * WinningResult.FOUR.getPrizeAmount();
+        prize += lottosWonFive * WinningResult.FIVE.getPrizeAmount();
+        prize += lottosWonFiveAndBonus * WinningResult.FIVE_AND_BONUS.getPrizeAmount();
+        prize += lottosWonSix * WinningResult.SIX.getPrizeAmount();
+        return prize;
+    }
 
 }
