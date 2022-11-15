b4301828d3d4dfdd10c3eec41729cbe345e9794f
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:48:22 +0900

    feat: 로또 당첨/보너스 번호를 선택 및 반환하는 기능 구현
    LottoNumberGetter의 getLottoNumber 메서드를 구현함.
    - 해당 메서드는 내부적으로 당첨/보너스 번호 입력을 요청하는 메서드, 입력을 받아오는 메서드, 입력을 검증하는 메서드를 호출함.
    - 호출 중, 입력이 유효하지 않으면 예외를 발생시켜 해당 메서드의 호출자에게 throw 함.
    - 모든 입력이 유효할 경우, LottoNumber 타입의 개체에 당첨 번호(Lotto 타입으로 저장)와 보너스 번호를 담아 메서드 호출자에 반환함.

--- src/main/java/lotto/lottonumber/LottoNumberGetter.java
+++ src/main/java/lotto/lottonumber/LottoNumberGetter.java
@@ -1,6 +1,8 @@
 package lotto.lottonumber;
 
 
+import lotto.data.Lotto;
+import lotto.data.LottoNumber;
 import lotto.lottonumber.bonusnumber.BonusNumberValidationCode;
 import lotto.lottonumber.bonusnumber.BonusNumberValidator;
 import lotto.lottonumber.winningnumber.WinningNumberValidationCode;
@@ -9,6 +11,7 @@ import lotto.lottonumber.winningnumber.WinningNumberValidator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -22,6 +25,22 @@ public class LottoNumberGetter {
     public LottoNumberGetter() {
         this.winningNumberValidator = new WinningNumberValidator();
         this.bonusNumberValidator = new BonusNumberValidator();
+    }
+
+    public LottoNumber getLottoNumber() {
+        printPromptForWinningNumbers();
+        List<String> tokensForWinningNumbers = tokenizeWinningNumberInput(getWinningNumbersFromConsole());
+        throwExceptionIfNotValidWinningNumbers(tokensForWinningNumbers);
+        List<Integer> winningNumbers = convertTokensToWinningNumbers(tokensForWinningNumbers);
+
+        System.out.println();
+
+        printPromptForBonusNumber();
+        String tokenForBonusNumber = getBonusNumberFromConsole();
+        throwExceptionIfNotValidBonusNumber(tokenForBonusNumber, winningNumbers);
+        int bonusNumber = Integer.parseInt(tokenForBonusNumber);
+
+        return new LottoNumber(new Lotto(winningNumbers), bonusNumber);
     }
 
     public void printPromptForWinningNumbers() {
@@ -63,5 +82,9 @@ public class LottoNumberGetter {
         if (result != BonusNumberValidationCode.VALID) {
             throw new IllegalArgumentException(result.getErrorMessage());
         }
+    }
+
+    private List<Integer> convertTokensToWinningNumbers(List<String> tokens) {
+        return tokens.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
 }
