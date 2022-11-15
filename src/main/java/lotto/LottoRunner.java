185bea21b9b3560c9f4240f10656413d3daeb284
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 10:38:16 +0900

    feat: LottoRunner 멤버변수 추가, 생성자 구현

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -1,6 +1,18 @@
 package lotto;
 
 public class LottoRunner {
+    private final UserInputGetter userInputGetter;
+    private final UserInputValidator userInputValidator;
+    private final PurchaseResultDemonstrator purchaseResultDemonstrator;
+    private final PurchaseResultProcessor purchaseResultProcessor;
+
+    private LottoRunner() {
+        this.userInputGetter = new UserInputGetter();
+        this.userInputValidator = new UserInputValidator();
+        this.purchaseResultDemonstrator = new PurchaseResultDemonstrator();
+        this.purchaseResultProcessor = new PurchaseResultProcessor();
+    }
+
     public void run() {
 
     }
