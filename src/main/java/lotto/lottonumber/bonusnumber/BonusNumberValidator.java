de87b8a096cd9f35b555bfb84e3448c4f76ab70a
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:42:21 +0900

    feat: 보너스 번호 입력의 유효성 결과를 반환하는 기능 구현
    BonusNumberValidator validateBonusNumber() 메서드를 구현함.
    - 해당 메서드는 내부적으로 세부 검증 기능을 수행하는 다른 메서드를 호출하고, 결과에 따라 BonusNumberValidationCode 열거형 타입의 검증 코드를 호출자(LottoNumberGetter)에게 반환함.

--- src/main/java/lotto/lottonumber/bonusnumber/BonusNumberValidator.java
+++ src/main/java/lotto/lottonumber/bonusnumber/BonusNumberValidator.java
@@ -3,6 +3,19 @@ package lotto.lottonumber.bonusnumber;
 import java.util.List;
 
 public class BonusNumberValidator {
+    public BonusNumberValidationCode validateBonusNumber(String token, List<Integer> winningNumbers) {
+        if (!isTokenNumber(token) || !isNumberFormatValid(token)) {
+            return BonusNumberValidationCode.INVALID_FORMAT;
+        }
+        int bonusNumber = Integer.parseInt(token);
+        if (!isNumberInRange(bonusNumber)) {
+            return BonusNumberValidationCode.NUMBER_OUT_OF_RANGE;
+        }
+        if (!isNumberDistinctFromWinningNumbers(bonusNumber, winningNumbers)) {
+            return BonusNumberValidationCode.DUPLICATE_EXIST_IN_WINNING_NUMBERS;
+        }
+        return BonusNumberValidationCode.VALID;
+    }
 
     public boolean isTokenNumber(String token) {
         try {
