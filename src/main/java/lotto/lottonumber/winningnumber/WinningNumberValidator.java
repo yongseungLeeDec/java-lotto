6edf3e6b351effe580db0c713ea0bbf12bdd510a
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:27:58 +0900

    feat: 입력된 당첨번호가 총 6개 토큰인지 여부 확인 기능 추가
    WinningNumberValidator hasExactlySixTokens() 메서드를 구현함.
    - 해당 메서드는 LottoNumberGetter에서 넘겨준 토큰 목록의 토큰이 총 6개인지 확인함.
    - 단, 토큰이 올바른 형식과 범위의, 중복 없는 숫자인지는 확인하지 않음.

--- src/main/java/lotto/lottonumber/winningnumber/WinningNumberValidator.java
+++ src/main/java/lotto/lottonumber/winningnumber/WinningNumberValidator.java
@@ -1,5 +1,10 @@
 package lotto.lottonumber.winningnumber;
 
+import java.util.List;
+
 public class WinningNumberValidator {
 
+    public boolean hasExactlySixTokens(List<String> tokens) {
+        return tokens.size() == 6;
+    }
 }
