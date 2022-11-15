5438cb9e1860682cb88896f68f39c686ed118804
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:39:15 +0900

    feat: 보너스 번호 입력이 숫자인지 확인하는 기능 구현
    BonusNumberValidator의 isTokenNumber 메서드를 구현함.

--- src/main/java/lotto/lottonumber/bonusnumber/BonusNumberValidator.java
+++ src/main/java/lotto/lottonumber/bonusnumber/BonusNumberValidator.java
@@ -2,4 +2,12 @@ package lotto.lottonumber.bonusnumber;
 
 public class BonusNumberValidator {
 
+    public boolean isTokenNumber(String token) {
+        try {
+            Integer.parseInt(token);
+        } catch (NumberFormatException exception) {
+            return false;
+        }
+        return true;
+    }
 }
