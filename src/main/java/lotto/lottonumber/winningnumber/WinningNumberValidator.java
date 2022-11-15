75af8db288f8063fe72983716ac77741b8b0ea59
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:29:38 +0900

    feat: 입력된 당첨번호 토큰이 모두 숫자인지 확인하는 기능 구현
    WinningNumberValidator hasNumberElementsOnly() 메서드를 구현함.
    - 해당 메서드의 도우미 함수인 isNumericExpression 함수도 함께 구현함.
    - 해당 메서드는 LottoNumberGetter에서 넘겨준 토큰 목록의 토큰들이 모두 숫자로 변환 가능한 문자열인지 확인함.
    - 단, 0으로 시작하거나 +, - 부호가 붙은 숫자인지 여부는 확인하지 않음.

--- src/main/java/lotto/lottonumber/winningnumber/WinningNumberValidator.java
+++ src/main/java/lotto/lottonumber/winningnumber/WinningNumberValidator.java
@@ -7,4 +7,17 @@ public class WinningNumberValidator {
     public boolean hasExactlySixTokens(List<String> tokens) {
         return tokens.size() == 6;
     }
+
+    public boolean hasNumberElementsOnly(List<String> tokens) {
+        return tokens.stream().filter(this::isNumericExpression).count() == tokens.size();
+    }
+
+    private boolean isNumericExpression(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch (NumberFormatException exception) {
+            return false;
+        }
+        return true;
+    }
 }
