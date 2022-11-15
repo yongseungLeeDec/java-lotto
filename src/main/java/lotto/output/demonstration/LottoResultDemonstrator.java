0e56ccda45d01db8a66e11c77f445da944f08369
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 20:13:39 +0900

    feat: 당첨 통계 출력 기능 구현
    LottoResultDemonstrator의 printLottoResult 메서드를 구현함.
    해당 메서드는 LottoResultEvaluator가 생성해서 반환하는 LottoResult 개체를 받아, 당첨 통계 제목 및 구분선과 함께 콘솔에 출력함.

--- src/main/java/lotto/output/demonstration/LottoResultDemonstrator.java
+++ src/main/java/lotto/output/demonstration/LottoResultDemonstrator.java
@@ -1,8 +1,16 @@
 package lotto.output.demonstration;
 
+import lotto.data.LottoResult;
+
 public class LottoResultDemonstrator {
     private static final String HEADING = "당첨 통계";
     private static final String BREAK_LINE = "---";
+
+    public void printLottoResult(LottoResult lottoResult) {
+        printHeading();
+        printBreakLine();
+        System.out.println(lottoResult);
+    }
 
     private void printHeading() {
         System.out.println(HEADING);
