b4c376ee01cb66e177e66a0cb36de957064c61d6
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 11:46:21 +0900

    feat: 로또 구매결과 출력 기능 구현
    PurchaseResultDemonstrator의 printPurchaseResult 구현함.
    - 본 메서드는 새로운 기능을 구현하는 것이 아니며, 기존의 (1) 로또 구매 개수 출력 기능, (2) 구매한 개수만큼 로또 번호를 출력하는 기능을 수행한 결과를 한꺼번에 합쳐서 수행하는 기능만을 함.

--- src/main/java/lotto/PurchaseResultDemonstrator.java
+++ src/main/java/lotto/PurchaseResultDemonstrator.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class PurchaseResultDemonstrator {
     private static final String PURCHASED_AMOUNT_NOTICE = "개를 구매했습니다.";
 
+    public void printPurchaseResult(PurchaseResult purchaseResult) {
+        printPurchasedAmount(purchaseResult.getPurchasedAmount());
+        printPurchasedLottos(purchaseResult.getPurchasedLottos());
+    }
+
     public void printPurchasedAmount(int purchasedAmount) {
         System.out.println(purchasedAmount + PURCHASED_AMOUNT_NOTICE);
     }
