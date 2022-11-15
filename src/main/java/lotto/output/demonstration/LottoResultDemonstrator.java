bcf4282f7f2a7861bde5626e18a4ac873c408c55
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 19:53:38 +0900

    feat: 당첨 통계 헤딩(제목), 구분선 출력 기능 추가

--- src/main/java/lotto/output/demonstration/LottoResultDemonstrator.java
+++ src/main/java/lotto/output/demonstration/LottoResultDemonstrator.java
@@ -1,5 +1,14 @@
 package lotto.output.demonstration;
 
 public class LottoResultDemonstrator {
+    private static final String HEADING = "당첨 통계";
+    private static final String BREAK_LINE = "---";
 
+    private void printHeading() {
+        System.out.println(HEADING);
+    }
+
+    private void printBreakLine() {
+        System.out.println(BREAK_LINE);
+    }
 }
