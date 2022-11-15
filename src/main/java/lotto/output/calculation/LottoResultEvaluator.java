30f15228d1440c33cce9d50fb75add30672ec348
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 19:59:57 +0900

    feat: 로또별 맞춘 숫자를 세어 반환하는 기능 구현
    LottoResultEvaluator 클래스의 LottoResultEvaluator를 구현함.
    - 해당 클래스는 구매한 로또 개체 하나와, 당첨/보너스 번호를 대조하여, 맞춘 번호의 개수를 반환함.
    - 단, 숫자 다섯개가 일치하고 보너스 번호가 일치하는 경우에는, '7'을 반환함.

--- src/main/java/lotto/output/calculation/LottoResultEvaluator.java
+++ src/main/java/lotto/output/calculation/LottoResultEvaluator.java
@@ -1,5 +1,24 @@
 package lotto.output.calculation;
 
-public class LottoResultEvaluator {
+import lotto.data.Lotto;
+import lotto.data.LottoNumber;
 
+import java.util.List;
+
+public class LottoResultEvaluator {
+    private static final int NUMBER_TO_EXPRESS_FIVE_AND_BONUS_WIN = 7;
+
+    public int LottoResultEvaluator(LottoNumber lottoNumber, Lotto purchasedLotto) {
+        List<Integer> winningNumbers = lottoNumber.getLotto().getNumbers();
+        int bonusNumber = lottoNumber.getBonusNumber();
+        List<Integer> numbers = purchasedLotto.getNumbers();
+
+        int matchCount = (int) numbers.stream().filter(winningNumbers::contains).count();
+
+        if (matchCount == 5 && numbers.contains(bonusNumber)) {
+            matchCount = NUMBER_TO_EXPRESS_FIVE_AND_BONUS_WIN;
+        }
+
+        return matchCount;
+    }
 }
