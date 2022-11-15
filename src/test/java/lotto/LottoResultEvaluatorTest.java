e4eeb5e40fa381446fef8e3346d6ce8e3c7b9262
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 20:57:24 +0900

    test: 당첨금 총액 계산 기능 테스트

--- src/test/java/lotto/LottoResultEvaluatorTest.java
+++ src/test/java/lotto/LottoResultEvaluatorTest.java
@@ -2,7 +2,9 @@ package lotto;
 
 import lotto.data.Lotto;
 import lotto.data.LottoNumber;
+import lotto.data.LottoResult;
 import lotto.data.PurchaseResult;
+import lotto.data.WinningResult;
 import lotto.output.calculation.LottoResultEvaluator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -126,5 +128,39 @@ class LottoResultEvaluatorTest {
         for (int i = 0; i < actualResults.length; i++) {
             assertThat(actualResults[i]).isEqualTo(expectedResults[i]);
         }
+    }
+
+    @DisplayName("LottoResult가 상금 액수를 올바르게 반환한다")
+    @Test
+    void lottoResultGetPrizeTest() {
+        LottoResultEvaluator lottoResultEvaluator = new LottoResultEvaluator();
+        LottoNumber lottoNumber = new LottoNumber(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+        Lotto purchasedLotto0 = new Lotto(List.of(40, 3, 42, 4, 44, 5)); // 3
+        Lotto purchasedLotto1 = new Lotto(List.of(40, 41, 42, 43, 44, 45)); // 0
+        Lotto purchasedLotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7)); // 7
+        Lotto purchasedLotto3 = new Lotto(List.of(40, 42, 44, 5, 1, 2)); // 3
+        Lotto purchasedLotto4 = new Lotto(List.of(40, 7, 1, 2, 3, 6)); // 4
+        Lotto purchasedLotto5 = new Lotto(List.of(9, 10, 11, 12, 13, 14)); // 0
+
+        List<Lotto> purchasedLottos = new ArrayList<>();
+        purchasedLottos.add(purchasedLotto0);
+        purchasedLottos.add(purchasedLotto1);
+        purchasedLottos.add(purchasedLotto2);
+        purchasedLottos.add(purchasedLotto3);
+        purchasedLottos.add(purchasedLotto4);
+        purchasedLottos.add(purchasedLotto5);
+
+        PurchaseResult purchaseResult = new PurchaseResult(6, purchasedLottos);
+
+        int[] winningResults = new int[purchaseResult.getPurchasedAmount()];
+
+        for (int i = 0; i < winningResults.length; i++) {
+            winningResults[i] = lottoResultEvaluator.countMatchingNumbers(lottoNumber, purchasedLottos.get(i));
+        }
+        int[] actualResults = lottoResultEvaluator.countLottosByWinningResult(winningResults);
+
+        LottoResult lottoResult = new LottoResult(actualResults);
+        assertThat(lottoResult.getTotalPrizeAmount()).isEqualTo(2 * WinningResult.THREE.getPrizeAmount() +
+                1 * WinningResult.FOUR.getPrizeAmount() + 1 * WinningResult.FIVE_AND_BONUS.getPrizeAmount());
     }
 }
