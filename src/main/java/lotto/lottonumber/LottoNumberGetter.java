42f3f2829c67b2dcd9f9985909bee7816b56e57c
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-15 16:17:35 +0900

    feat: 콘솔에서 당첨 번호 입력을 요청하고 받는 기능 구현
    LottoNumberGetter의 printPromptForWinningNumbers 메서드를 구현함
    - 콘솔에 당첨 번호 입력을 요청하는 기능
    LottoNumberGetter의 getWinningNumbersFromConsole 메서드를 구현함
    - 콘솔에 당첨 번호 입력을 받아오는 기능

--- src/main/java/lotto/lottonumber/LottoNumberGetter.java
+++ src/main/java/lotto/lottonumber/LottoNumberGetter.java
@@ -1,8 +1,20 @@
 package lotto.lottonumber;
 
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class LottoNumberGetter {
+    private static final String PROMPT_FOR_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+
     public LottoNumberGetter() {
 
+    }
+
+    public void printPromptForWinningNumbers() {
+        System.out.println(PROMPT_FOR_WINNING_NUMBERS);
+    }
+
+    public String getWinningNumbersFromConsole() {
+        return readLine();
     }
 }
